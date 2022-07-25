f5ecced55cb1a847c22076657294caeec51b89a8
Ayush Agarwal <95439195+ayush-agarwal-190@users.noreply.github.com>
2022-07-25 16:34:30 +0530

    Add files via upload

---
+++ insert.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+void insertAtTail(node* &head, int val)
+{
+    
+ node* n=new node(val);
+  if(head==NULL)
+  {
+    head=n;
+    return;
+  }
+    node* temp=head;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=n;
+
+}
+void insertAtHead(node* &head, int val)
+{
+    node* n=new node(val);
+    n->next=head;
+    head=n;
+
+
+}
+void display(node* head)
+{
+    node* temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+int main()
+{
+     node* head=NULL;
+     insertAtTail(head,1);
+     insertAtTail(head,7);
+     insertAtTail(head,18);
+     insertAtTail(head,31);
+     insertAtHead(head,1);
+     insertAtHead(head,7);
+     insertAtHead(head,18);
+     insertAtHead(head,31);
+     display(head);
+}
---
+++ reverseRecursive.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node* next;
+
+    node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+void insertAtTail(node* &head, int val)
+{
+    
+ node* n=new node(val);
+  if(head==NULL)
+  {
+    head=n;
+    return;
+  }
+    node* temp=head;
+    while(temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=n;
+
+}
+node* reverseRecursive(node* &head)
+{
+    if(head==NULL || head->next==NULL)
+    {
+        return head;
+    }
+    node* newhead=reverseRecursive(head->next);
+    head->next->next=head;
+    head->next=NULL;
+
+    return newhead;
+}
+
+void display(node* head)
+{
+    node* temp=head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+int main()
+{
+     node* head=NULL;
+     insertAtTail(head,1);
+     insertAtTail(head,7);
+     insertAtTail(head,18);
+     insertAtTail(head,31);
+     display(head);
+     node* newhead=reverseRecursive(head);
+     display(newhead);
+
+     return 0;
+}
